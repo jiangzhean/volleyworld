111869d71bd169305a66c986752fbe1f1d73caf3
jiangzhean <112997913+jiangzhean@users.noreply.github.com>
2023-12-31 17:31:49 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VolleyWorld - 排球世界</title>
+    <style>
+        /* Add your custom styles here */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        header {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 1em;
+        }
+
+        nav {
+            background-color: #555;
+            padding: 1em;
+            text-align: center;
+        }
+
+        nav a {
+            color: white;
+            text-decoration: none;
+            padding: 0.5em 1em;
+            margin: 0 1em;
+        }
+
+        section {
+            padding: 2em;
+        }
+
+        footer {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 1em;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <h1>VolleyWorld - 排球世界</h1>
+    </header>
+
+    <nav>
+        <a href="#home">首頁</a>
+        <a href="#news">新聞</a>
+        <a href="#matches">比賽</a>
+    </nav>
+
+    <section id="home">
+        <h2>歡迎來到 VolleyWorld</h2>
+        <p>這裡是您了解排球世界的最佳地方。獲取最新的新聞和比賽資訊。</p>
+    </section>
+
+    <section id="news">
+        <h2>最新新聞</h2>
+        <ul>
+            <li><a href="#">排球世錦賽即將展開</a></li>
+            <li><a href="#">球隊動態：球員專訪</a></li>
+            <li><a href="#">全新排球裝備推介</a></li>
+        </ul>
+    </section>
+
+    <section id="matches">
+        <h2>即將舉行的比賽</h2>
+        <ul>
+            <li><a href="#">2023 年度排球錦標賽</a></li>
+            <li><a href="#">國際排球聯賽</a></li>
+        </ul>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 VolleyWorld. All rights reserved.</p>
+    </footer>
+</body>
+
+</html>
